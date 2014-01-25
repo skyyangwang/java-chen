9ad702c8f295214f27bc463ca5834d1ae3997167
skyyangwang <sky_yangwang@sina.com>
2014-01-25 15:55:16 +0800

    2014-0125---1

--- 4移动端/mina/c  java 通讯/CtoJava.java
+++ 4移动端/mina/c  java 通讯/CtoJava.java
@@ -33,6 +33,30 @@ public class CtoJava {
         
     }
 	
+/*	public static StoJava getStoJava(byte[] bArr) {  
+		String phoneid = "";  
+	    String routeid = "";  
+	    int method = 0;  
+        
+	    //声明要提取字段的字节最大长度；
+        byte[] temp1 = new byte[16];  
+        byte[] temp2 = new byte[32]; 
+        byte[] temp3 = new byte[4]; 
+        
+        //bArr-全字节数组；24-本字节开始坐标；temp-存放本字节的字节数组；0-本字节相对起始坐标；16-本字节最大坐标长度；
+        System.arraycopy(bArr, 24, temp1, 0, 16);  
+        phoneid = toStr(temp1,16);
+        
+        System.arraycopy(bArr, 40, temp2, 0, 32);  
+        routeid = toStr(temp2,32);
+        
+        System.arraycopy(bArr, 72, temp3, 0, 4);  
+        method = vtolh(temp3);  
+          
+        return new StoJava(phoneid, routeid, method);  
+        
+    }*/
+	
 	/** 
      * 将byte数组转化成String 
      */  
