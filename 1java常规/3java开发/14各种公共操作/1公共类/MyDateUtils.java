8f7156c9eb9d5e777893374df5f696e271f1180a
skyyangwang <sky_yangwang@sina.com>
2014-09-30 17:07:51 +0800

    2014-0930---1

--- 1java常规/3java开发/14各种公共操作/1公共类/MyDateUtils.java
+++ 1java常规/3java开发/14各种公共操作/1公共类/MyDateUtils.java
@@ -7,22 +7,18 @@ package com.oa.product.action;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.regex.Pattern;
 
-import org.apache.commons.lang.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
 
 /**
- * 日期工具类, 继承org.apache.commons.lang.time.DateUtils类
+ * 日期工具类
  * 
- * @author ThinkGem,chenpeng
- * @version 2013-3-15,2014-09-29
+ * @author chenpeng
+ * @version 2014-09-29
  */
-public class DateUtils extends org.apache.commons.lang.time.DateUtils {
-
-	private static Calendar calS = Calendar.getInstance();
-	private static Pattern p = Pattern.compile("\\d{4}-\\d{2}-\\d{2}");// 定义整则表达式
+public class MyDateUtils {
 
 	private static String[] parsePatterns = { "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm",
 			"yyyy/MM/dd", "yyyy/MM/dd HH:mm:ss", "yyyy/MM/dd HH:mm" };
@@ -76,7 +72,7 @@ public class DateUtils extends org.apache.commons.lang.time.DateUtils {
 	}
 
 	/**
-	 * 得到当前星期字符串 格式（E）星期几
+	 * 得到当前星期字符串 格式（E）星期几| 其它的查下文档的关键字
 	 */
 	public static String getWeek() {
 		return formatDate(new Date(), "E");
@@ -91,7 +87,7 @@ public class DateUtils extends org.apache.commons.lang.time.DateUtils {
 			return null;
 		}
 		try {
-			return parseDate(str.toString(), parsePatterns);
+			return DateUtils.parseDate(str.toString(), parsePatterns);
 		} catch (ParseException e) {
 			return null;
 		}
@@ -108,6 +104,23 @@ public class DateUtils extends org.apache.commons.lang.time.DateUtils {
 		return t / (24 * 60 * 60 * 1000);
 	}
 
+	/**
+	 * 获取两个日期的相隔天数
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static long distanceDays(Date date1, Date date2) {
+		long t = date2.getTime() - date1.getTime();
+		return t / (24 * 60 * 60 * 1000);
+	}
+
+	/**
+	 * 获取某天的开始时间，格式"yyyy-MM-dd HH:mm:ss"
+	 * 
+	 * @param date
+	 * @return
+	 */
 	public static Date getDateStart(Date date) {
 		if (date == null) {
 			return null;
@@ -121,6 +134,12 @@ public class DateUtils extends org.apache.commons.lang.time.DateUtils {
 		return date;
 	}
 
+	/**
+	 * 获取某天的结束时间，格式"yyyy-MM-dd HH:mm:ss"
+	 * 
+	 * @param date
+	 * @return
+	 */
 	public static Date getDateEnd(Date date) {
 		if (date == null) {
 			return null;
@@ -132,66 +151,6 @@ public class DateUtils extends org.apache.commons.lang.time.DateUtils {
 			e.printStackTrace();
 		}
 		return date;
-	}
-
-	/*****
-	 * 返回相差年月日
-	 */
-	public static String remainDateToString(String startDateStr, String endDateStr) {
-		java.util.Date startDate = null;
-		java.util.Date endDate = null;
-		try {
-			startDate = new SimpleDateFormat("yyyy-MM-dd").parse(startDateStr);
-		} catch (ParseException e) {
-			e.printStackTrace();
-			return "";
-		}
-		try {
-			endDate = new SimpleDateFormat("yyyy-MM-dd").parse(endDateStr);
-		} catch (ParseException e) {
-			e.printStackTrace();
-			return "";
-		}
-		calS.setTime(startDate);
-		int startY = calS.get(Calendar.YEAR);
-		int startM = calS.get(Calendar.MONTH);
-		int startD = calS.get(Calendar.DATE);
-		int startDayOfMonth = calS.getActualMaximum(Calendar.DAY_OF_MONTH);
-
-		calS.setTime(endDate);
-		int endY = calS.get(Calendar.YEAR);
-		int endM = calS.get(Calendar.MONTH);
-		// 处理2011-01-10到2011-01-10，认为服务为一天
-		int endD = calS.get(Calendar.DATE) + 1;
-		int endDayOfMonth = calS.getActualMaximum(Calendar.DAY_OF_MONTH);
-
-		StringBuilder sBuilder = new StringBuilder();
-		if (endDate.compareTo(startDate) < 0) {
-			return sBuilder.append("0").toString();
-		}
-		int lday = endD - startD;
-		if (lday < 0) {
-			endM = endM - 1;
-			lday = startDayOfMonth + lday;
-		}
-		// 处理天数问题，如：2011-01-01 到 2013-12-31 2年11个月31天 实际上就是3年
-		if (lday == endDayOfMonth) {
-			endM = endM + 1;
-			lday = 0;
-		}
-		int mos = (endY - startY) * 12 + (endM - startM);
-		int lyear = mos / 12;
-		int lmonth = mos % 12;
-		if (lyear > 0) {
-			sBuilder.append(lyear);
-		}
-		// if (lmonth > 0) {
-		// sBuilder.append(lmonth+"-");
-		// }
-		// if (lday >0 ) {
-		// sBuilder.append(lday+"-");
-		// }
-		return sBuilder.toString();
 	}
 
 }
